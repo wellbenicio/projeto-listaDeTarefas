c826dbdb494c5dcac6e8e5a903b78d9c4e1d1bbc
Wellynton Benicio <well.03281@gmail.com>
2021-08-01 01:06:42 -0300

    Versão 1 - Lista de Tarefas

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+//função que adiciona tarefa: 
+
+function addTask() {
+    const taskTitle = document.querySelector("#task-title").value;
+
+    if (taskTitle) {
+        const template = document.querySelector(".template");
+        
+        const newTask = template.cloneNode(true);
+
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function() {
+            removeTask(this);
+        });
+
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function (){
+            completeTask(this);
+        });
+
+
+
+
+        document.querySelector("#task-title").value = "";
+
+    }
+
+}
+
+
+function removeTask(task) {
+    task.parentNode.remove(true);
+}
+
+
+function completeTask(task) {
+
+    const taskComplete = task.parentNode;
+
+    taskComplete.classList.toggle("done");
+}
+
+
+
+// Evento de adicionar tarefa:
+
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function(e) {
+    
+    e.preventDefault();
+
+    addTask();
+});
